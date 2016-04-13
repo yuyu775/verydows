d7fc44f38a483afe72f4db4b478c29b17dc38686
cigery <150526401@qq.com>
2016-04-13 23:13:38 +0800

    修正授权登录中防止CSRF攻击参数state被覆盖问题

--- protected/model/oauth_model.php
+++ protected/model/oauth_model.php
@@ -8,11 +8,12 @@ class oauth_model extends Model
         $res = array();
         if($find_all = $this->find_all(array('enable' => 1))) 
         {
+            $_SESSION['oauth']['state'] = md5(uniqid(rand(), TRUE));
             foreach($find_all as $v)
             {
                 $res[$v['party']] = $v;
                 $oauth_obj = plugin::instance('oauth', $v['party'], array($v['params']), TRUE);
-                $res[$v['party']]['login_url'] = $oauth_obj->create_login_url();
+                $res[$v['party']]['login_url'] = $oauth_obj->create_login_url($_SESSION['oauth']['state']);
                 unset($res[$v['party']]['party']);
             }
         }
