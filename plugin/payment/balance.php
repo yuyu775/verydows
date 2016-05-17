19d6b362c5eccf30b600ca88dce57870b0b732d5
cigery <150526401@qq.com>
2016-05-17 20:29:44 +0800

    修正余额支付插件中的参数错误及付款状态未更新问题

--- plugin/payment/balance.php
+++ plugin/payment/balance.php
@@ -29,10 +29,14 @@ class balance extends abstract_payment
             $account = $account_model->find(array('user_id' => $args['user_id']), null, 'balance');
             if($account['balance'] >= $order['order_amount'])
             {
-                $account_model->decr(array('user_id' => $user_id), 'balance', $order['order_amount']);
-                $this->save_trade_log($user_id, $order_id, $order['order_amount']);
+                if($account_model->decr(array('user_id' => $args['user_id']), 'balance', $order['order_amount']))
+                {
+                    $order_model->update(array('order_id' => $args['order_id']), array('order_status' => 2));
+                    $this->save_trade_log($args['user_id'], $args['order_id'], $order['order_amount']);
+                    return array('success', '付款成功', url('order', 'view', array('id' => $args['order_id'])));
+                }
                 
-                return array('success', '付款成功', url('order', 'view', array('id' => $args['order_id'])));
+                return array('error', '付款失败，请重试', url('pay', 'index', array('order_id' => $args['order_id'])));
             }
             
             return array('error', '您的账户中余额不足，请使用其他支付方式', url('pay', 'index', array('order_id' => $args['order_id'])));
@@ -49,7 +53,7 @@ class balance extends abstract_payment
             'user_id' => $user_id,
             'balance' => 0 - $order_amount,
             'cause' => "使用账户余额支付订单[{$order_id}]",
-            'created_date' => $_SERVER['REQUEST_TIME'],
+            'dateline' => $_SERVER['REQUEST_TIME'],
         );
         $log_model->create($log_data);
     }
